a35e066dc246b4ad8ed5f158ea9a75c96e166d2c
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-07-24 14:04:37 +0700

    fix documentation

--- client/src/App.js
+++ client/src/App.js
@@ -7,41 +7,21 @@ import StoryThumbnail from "./components/StoryThumbnail";
 import Story from "./components/Story";
 import Prices from "./components/Prices";
 import Footer from "./layouts/Footer";
-import {
-  Navbar,
-  Nav,
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-  Stack,
-  Card,
-  CardGroup,
-} from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import "./App.css";
-import sample from "./assets/sample.jpeg";
 
 function App() {
   return (
     <div className="App">
-      {/* Navbar */}
       <NavigationBar />
       <Container fluid="md mt-5 mb-5">
-        {/* Registration */}
         <RegistrationForm />
-        {/* Login Form */}
         <LoginFrom />
-        {/* Post */}
         <PostStory />
-        {/* Index Story lists */}
         <StoryThumbnail />
-        {/* Story */}
         <Story />
-        {/* Prices */}
         <Prices />
       </Container>
-      {/* footer */}
       <Footer />
     </div>
   );
