5f5ff7500305a7530f6a36aafec6ddd260790a94
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-12 17:21:32 +0700

    added Bio and fixed row into appropriate react-bootstrap

--- client/src/pages/RegisterUser/RegisterUser.jsx
+++ client/src/pages/RegisterUser/RegisterUser.jsx
@@ -26,54 +26,53 @@ function RegisterUser() {
     <Container className="mt-5 mb-5">
       <h1>BlackJack</h1>
       <Form>
-        <Form.Group className="mb-3" controlId="formName">
-          <Row>
-            <Col>
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="First Name" />
-            </Col>
-            <Col>
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" placeholder="Last Name" />
-            </Col>
-          </Row>
+        <Form.Group as={Row} className="mb-3" controlId="formName">
+          <Col>
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="First Name" />
+          </Col>
+          <Col>
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Last Name" />
+          </Col>
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formEmail">
-          <Row>
-            <Col lg={6}>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Col>
-          </Row>
+        <Form.Group as={Row} className="mb-3" controlId="formEmail">
+          <Col lg={6}>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Col>
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="forUsername">
-          <Row>
-            <Col md={6}>
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placehodler="username" />
-            </Col>
-          </Row>
+        <Form.Group as={Row} className="mb-3" controlId="forUsername">
+          <Col md={6}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placehodler="username" />
+          </Col>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formPassword">
-          <Row>
-            <Col md={6}>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Col>
-            <Col md={6}>
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Col>
-          </Row>
+        <Form.Group as={Row} className="mb-3" controlId="formPassword">
+          <Col md={6}>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
+          <Col md={6}>
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Col>
         </Form.Group>
-
+        <Form.Group className="mb-3">
+          <Form.Label>Bio</Form.Label>
+          <Form.Control
+            as="textarea"
+            placeholder="Present yourself"
+            controlId="Bio"
+            style={{ height: "100px" }}
+          />
+          <Form.Text className="text-muted">Maximum 200 words</Form.Text>
+        </Form.Group>
         <Form.Group as={Row} className="mb-3" controlId="socialMedia">
-          <h3>Social Media</h3>
+          <h3 className="mb-3">Social Media</h3>
           <Form.Label className="mb-3" column sm="1">
             Twitter
           </Form.Label>
