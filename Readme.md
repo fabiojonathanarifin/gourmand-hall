c428fda9e984e39f322c3e55760fe2b6c2dc0cc6
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-17 20:06:00 +0700

    Dev Notes created in the readme file

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Developer Notes:
+
+Client
+- Put Story thumbnail card on page to lay the stories out properly prior to the App.js
+    - Basically make another layer file as the parent of storyThumbnail to display multiple stories.
--- client/src/App.js
+++ client/src/App.js
@@ -17,42 +17,26 @@ import { getQuote, getIndex } from "./api/index";
 function App() {
   // const [users, setUsers] = useState([]);
   const [stories, setStories] = useState([]);
-  //use useEffect to display storyThumbnail
-  //specifically use useEffect to fetch the data (fetch in api>index, and import it here)
-  // & set up the render in StoryThumbnail.jsx to display the fetched data.
-  //the fetched data is the first(index 0) of the story
+
+  //getIndex is returning a promise
   const data = async () => {
     const response = await getIndex();
     console.log(response);
     setStories(response);
   };
 
+  //useEffect displaying the data right away
   useEffect(() => {
     data();
   }, []);
-  // const data = getIndex();
-  // console.log(data);
-  // setStories(data);
-  // useEffect(() => {
-  //   const btnOnClick = async () => {
-  //     let result = await getIndex();
-  //     setUsers(result);
-  //   };
-  // }, []);
-
   return (
     <div className="App">
       <NavigationBar />
       <Container className="contentmargin" fluid="md mb-5">
         {/* Use react-router */}
         <Button>Get Quote</Button>
-        {/* <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.title}</li>
-          ))}
-        </ul> */}
-        {/* <ul>{users.title}</ul> */}
         <Routes>
+          {/* data placed properly by using props on StoryThumbnail.jsx */}
           <Route
             path="/thumbnail"
             element={stories.map((story) => (
