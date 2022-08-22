b4f65e92a764d7290e9f351138ec9e4338d283c7
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-22 19:51:29 +0700

    clean up routing on app.js

--- server/app.js
+++ server/app.js
@@ -1,21 +1,13 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import mongoose from "mongoose";
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("./models/comment");
 
-// const createUser = require("./routes/user");
-const createUser = require("./routes/users");
-
 const storyRoutes = require("./routes/stories");
-const getStory = require("./routes/stories");
-const createStory = require("./routes/stories");
-// const showStory = require("./routes/stories");
+
+const userRoutes = require("./routes/users");
 const app = express();
 const cors = require("cors");
-const getIndex = require("./routes/stories");
 
 // const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/gourmand-hall";
 mongoose
@@ -28,11 +20,8 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
-app.use("/register", createUser);
-app.use("/userlist", getStory);
-app.use("/story", createStory);
+app.use("/", userRoutes);
 app.use("/", storyRoutes);
-app.use("/index", getIndex);
 
 app.get("/", (req, res) => {
   res.send("HELLO FROM GOURMAND HALL!");
