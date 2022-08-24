2d0563d353ab9c07490553270b7157cf452c9960
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-24 20:58:07 +0700

    passportJS login logic added

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,11 @@
 const User = require("../models/user");
 
 module.exports.createUser = async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-  res.send(user);
+  const { email, username, password } = req.body;
+  const user = new User({ email, username });
+  const registeredUser = await User.register(user, password);
+  console.log(registeredUser);
+  // const user = new User(req.body);
+  // await user.save();
+  // res.send(user);
 };
