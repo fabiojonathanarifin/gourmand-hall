986d1432d54cbcf5e9a524195a8774d1eab7f552
fabiojonathanarifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-24 20:23:49 +0700

    use passport for login and session in/out

--- server/app.js
+++ server/app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("./models/comment");
+const User = require("./models/user");
 
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 
 const storyRoutes = require("./routes/stories");
-
 const userRoutes = require("./routes/users");
+
 const app = express();
 const cors = require("cors");
 
@@ -21,6 +22,8 @@ mongoose
 //bodyParser allow the usage of req.body
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
 
 //session
 const sessionConfig = {
@@ -39,9 +42,16 @@ app.use(session(sessionConfig));
 //PassportJS
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy());
+//docs for passport and Local Strategy:
+// https://www.passportjs.org/howtos/password/
+//docs for User.authenticate():
+//www.npmjs.com/package/passport-local-mongoose
+passport.use(new LocalStrategy(User.authenticate()));
 
-app.use(cors());
+//store user activity in the session: passport-local-mongoose
+passport.serializeUser(User.serializeUser());
+//unstore user activity in the session: passport-local-mongoose
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/greet", (req, res) => {
   res.send("Hi Fellas");
