eae820314cfbaef1624cbf7bb39db55a11732f66
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-08-30 17:47:49 +0700

    Error condition for registering user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,10 +3,20 @@ const User = require("../models/user");
 module.exports.createUser = async (req, res) => {
   const { email, username, password } = req.body;
   const user = new User({ email, username });
-  const registeredUser = await User.register(user, password);
+  const registeredUser = await User.register(user, password, function (err) {
+    if (err) {
+      res.json({
+        success: false,
+        message: "Your account could not be saved. Error: ",
+        err,
+      });
+    } else {
+      res.json({ success: true, message: "Your account has been saved" });
+    }
+  });
   console.log(registeredUser);
 };
 
-module.exports.login = async (req, res) => {
-  console.log("login route accessed");
+module.exports.loginUser = async (req, res) => {
+  res.json({ status: "success" });
 };
