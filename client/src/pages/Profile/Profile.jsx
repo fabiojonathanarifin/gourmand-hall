126075b513992f496d7a1679ad5ea923e09a9c42
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-09-06 15:49:32 +0700

    setup Profile api call

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from "react";
 import { Card, Row, Col, Stack, Container } from "react-bootstrap";
 import "./Profile.css";
 import sample from "../../assets/sample.jpeg";
 import GeneralButton from "../../components/Buttons/Button/GeneralButton";
 import Index from "../Index/Index";
-
+import axios from "axios";
+import { getUser } from "../../api/index";
 function Profile() {
+  const [data, setData] = useState({});
+  const handleData = async () => {
+    const response = await getUser();
+    if ((response.success = false)) {
+      window.location.replace(response.redirectDestination);
+    }
+    setData(response.userData);
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
+  console.log(data);
   return (
     <div className="profilecard">
       <Stack gap={5}>
@@ -24,17 +39,19 @@ function Profile() {
                 </div>
               </Col>
               <Col md={8}>
-                <Card.Title>Fabio Jonathan Arifin</Card.Title>
+                <Card.Title>
+                  {data.firstName} {data.lastName}
+                </Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">
-                  Jakarta, Indonesia
+                  {data.location}
                 </Card.Subtitle>
-                <Card.Text>Email: fabiojonathanarifin@gmail.com</Card.Text>
+                <Card.Text>Email: {data.email}</Card.Text>
                 <Card.Text>Website: www.gourmandhall.com</Card.Text>
                 <Card.Text>Birthday: September 20, 1996</Card.Text>
                 <Card>
                   <Card.Body>
                     <Card.Subtitle className="mb-3">About me</Card.Subtitle>
-                    <Card.Text>I'm gourmet, yet an engineer.</Card.Text>
+                    <Card.Text>{data.bio}</Card.Text>
                   </Card.Body>
                 </Card>
                 <Stack className="mt-3" direction="horizontal" gap={3}>
