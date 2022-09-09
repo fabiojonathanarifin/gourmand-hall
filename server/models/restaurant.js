a25284169b4cda13ae8b25c3251a37ea36496a37
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-09-09 20:11:18 +0700

    change location to state & city on restaurant model

--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose;
 
 const RestaurantSchema = new Schema({
   name: { type: String, required: true },
-  location: { type: String, required: true },
+  state: { type: String, required: true },
+  city: { type: String, required: true },
   google: { type: String },
   website: { type: String },
   socialMedia: { type: String },
