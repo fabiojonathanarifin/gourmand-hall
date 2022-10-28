5c617bbb84270d3741842421975117d2a0dfc185
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-10-28 18:00:42 +0700

    updated likes in comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const CommentSchema = new Schema({
   author: [{ type: Schema.Types.ObjectId, ref: "User" }],
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  likes: Boolean,
+  likes: { type: Number, default: 0 },
   date: { type: Date, default: Date.now },
   hidden: Boolean,
 });
