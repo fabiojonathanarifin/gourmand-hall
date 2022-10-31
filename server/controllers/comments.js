bcb51b8d9da72e3a894f8fe588c56bcedb2094c8
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-10-31 17:48:04 +0700

    Fixed createComment BE: save comment and add it as User's reference

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -2,11 +2,23 @@ const User = require("../models/user");
 const Comment = require("../models/comment");
 
 module.exports.createComment = async (req, res) => {
-  const UserInSessionID = req.user.id;
-  const comment = new Comment(req.body);
+  const UserInSessionID = req.user._id;
+  console.log(UserInSessionID);
+  const comment = new Comment({
+    comment: req.body.comment,
+    author: UserInSessionID,
+  });
   //User has to be imported from the model
-  const user = await User.findByIdAndUpdate(UserInSessionID, comment);
-  await comment.save();
+  // const user = await User.findByIdAndUpdate(UserInSessionID, comment);
+  await comment.save(
+    await User.findByIdAndUpdate(req.user.id, {
+      $push: { comments: comment._id },
+    })
+      .populate("comments")
+      .exec((err, User) => {
+        if (!err) return console.log(User);
+      })
+  );
   console.log(comment);
 };
 
