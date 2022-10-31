7dc0ff2edbc2d00bc982dc718c2f03462a566d40
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-10-31 17:48:18 +0700

    Fixed comment model

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const CommentSchema = new Schema({
-  author: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  comment: { type: String },
   likes: { type: Number, default: 0 },
   date: { type: Date, default: Date.now },
   hidden: Boolean,
