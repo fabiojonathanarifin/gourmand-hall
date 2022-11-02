e07289420fcfcc2f2a4dae1bc3a68547f39017f5
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-02 16:21:26 +0700

    added story relation logic to comments controller

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const Comment = require("../models/comment");
+const Story = require("../models/story");
 
 module.exports.createComment = async (req, res) => {
   const UserInSessionID = req.user._id;
@@ -7,6 +8,7 @@ module.exports.createComment = async (req, res) => {
   const comment = new Comment({
     comment: req.body.comment,
     author: UserInSessionID,
+    story: req.body.story,
   });
   //User has to be imported from the model
   // const user = await User.findByIdAndUpdate(UserInSessionID, comment);
@@ -17,6 +19,13 @@ module.exports.createComment = async (req, res) => {
       .populate("comments")
       .exec((err, User) => {
         if (!err) return console.log(User);
+      }),
+    await Story.findByIdAndUpdate(req.body.story, {
+      $push: { comments: comment._id },
+    })
+      .populate("comments")
+      .exec((err, Story) => {
+        if (!err) return console.log(Story);
       })
   );
   console.log(comment);
