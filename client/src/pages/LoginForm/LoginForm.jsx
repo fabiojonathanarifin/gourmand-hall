651d20ff0fe1c8fc8a5a1e1af6a11128fdde8dc9
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-02 16:21:02 +0700

    cleaned up login form

--- client/src/pages/LoginForm/LoginForm.jsx
+++ client/src/pages/LoginForm/LoginForm.jsx
@@ -4,7 +4,6 @@ import { Form, Stack } from "react-bootstrap";
 import "./LoginForm.css";
 import GeneralButton from "../../components/Buttons/Button/GeneralButton";
 import { loginUser } from "../../api";
-// import { loginUser, getUser } from "../api";
 
 function LoginForm() {
   const [data, setData] = useState({
@@ -27,17 +26,12 @@ function LoginForm() {
   };
   return (
     <div className="centerItems mt-5">
-      {/* <div>
-        <h1>Get User</h1>
-        <button onClick={(e) => getUser(e.target.value)}>get User info</button>
-      </div> */}
       <Stack direction="horizontal" gap={3}>
         <div>
           <h1>GourmandHall</h1>
         </div>
         <Form onSubmit={(e) => handleSubmit(e)}>
           <Form.Group className="mb-3">
-            {/* <Form.Label>username</Form.Label> */}
             <Form.Control
               onChange={(e) => handleChange(e)}
               id="username"
@@ -46,9 +40,7 @@ function LoginForm() {
               placeholder="Username"
             />
           </Form.Group>
-
           <Form.Group className="mb-3">
-            {/* <Form.Label>Password</Form.Label> */}
             <Form.Control
               onChange={(e) => handleChange(e)}
               id="password"
@@ -57,11 +49,7 @@ function LoginForm() {
               placeholder="Password"
             />
           </Form.Group>
-          {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
           <GeneralButton Value="Submit" type="submit" />
-          {/* <Button variant="primary">Submit</Button> */}
         </Form>
       </Stack>
     </div>
