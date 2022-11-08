42401734451546ceb30b67989ef0664511ac287e
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-08 13:23:02 +0700

    added comment logic for processing author and comment text

--- client/src/pages/Story.jsx
+++ client/src/pages/Story.jsx
@@ -5,12 +5,14 @@ import { showStory, submitComment } from "../api";
 import { useParams } from "react-router-dom";
 import { getUser, getComments } from "../api";
 import GeneralButton from "../components/Buttons/Button/GeneralButton";
+import StoryComment from "./StoryComment/StoryComment";
 
 function Story() {
   const [story, setStory] = useState([]);
   const [commentData, setCommentData] = useState({
     comment: "",
   });
+  const [displayComments, setDisplayComments] = useState([]);
 
   let isLoggedIn = useRef();
 
@@ -32,7 +34,29 @@ function Story() {
 
   const handleComments = async () => {
     const response = await getComments(storyId);
-    console.log(response);
+    let commentDisplayData = [];
+    let index = 0;
+    while (response[index]) {
+      let commentAuthorArray = [response[index].author.firstName];
+      commentAuthorArray.push(response[index].author.lastName);
+      let commentAuthorName = commentAuthorArray.join(" ");
+      let commentText = response[index].comment;
+      commentDisplayData.push({
+        author: commentAuthorName,
+        comment: commentText,
+      });
+      index++;
+    }
+    console.log(commentDisplayData);
+    setDisplayComments(commentDisplayData);
+    // console.log(response);
+    // const commentAuthorArray = [
+    //   response[0].author.firstName,
+    //   response[0].author.lastName,
+    // ];
+    // const commentAuthor = commentAuthorArray.join(" ");
+    // const comment = response[0].comment;
+    // console.log(comment);
   };
 
   useEffect(() => {
@@ -63,10 +87,13 @@ function Story() {
           <Card.Text>{story.story}</Card.Text>
           {isLoggedIn.current ? <LikeButtons /> : "Please login to interact"}
         </Card.Body>
-        <Card>
+        {/* <Card>
           <Card.Subtitle className="mb-2 text-muted">Author</Card.Subtitle>
           <Card.Body>Comment</Card.Body>
-        </Card>
+        </Card> */}
+        {displayComments.map((comment) => (
+          <StoryComment key={comment._id} comment={comment} />
+        ))}
         {isLoggedIn.current ? (
           <Card.Footer>
             <Form onSubmit={(e) => handleSubmit(e)}>
