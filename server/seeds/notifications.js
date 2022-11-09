792361752c25ea3525f62afb0385a51c2b260205
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-09 10:18:08 +0700

    Added Notification

---
+++ server/seeds/notifications.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    event: "Password/Username Failed",
+    message: "Wrong password or username",
+  },
+  {
+    event: "Access Denied, Login",
+    message: "Please Login First",
+  },
+  {
+    event: "Access Denied, Admin",
+    message: "Please Login as an Admin",
+  },
+  {
+    event: "Incomplete Data",
+    message: "Please Complete the Form",
+  },
+];
