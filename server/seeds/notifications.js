f5d3138ba524bc7720b7ebd903decc60458b9b26
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-10 11:34:20 +0700

    notification database seed

--- server/seeds/notifications.js
+++ server/seeds/notifications.js
@@ -1,18 +1,20 @@
 module.exports = [
-  {
-    event: "Password/Username Failed",
-    message: "Wrong password or username",
-  },
-  {
-    event: "Access Denied, Login",
-    message: "Please Login First",
-  },
-  {
-    event: "Access Denied, Admin",
-    message: "Please Login as an Admin",
-  },
-  {
-    event: "Incomplete Data",
-    message: "Please Complete the Form",
-  },
+  { type: "SUCCESS", message: "Welcome to Gourmand Hall!" },
+  { type: "FAIL", message: "Please input the correct username/password" },
+  // {
+  //   event: "Password/Username Failed",
+  //   message: "Wrong password or username",
+  // },
+  // {
+  //   event: "Access Denied, Login",
+  //   message: "Please Login First",
+  // },
+  // {
+  //   event: "Access Denied, Admin",
+  //   message: "Please Login as an Admin",
+  // },
+  // {
+  //   event: "Incomplete Data",
+  //   message: "Please Complete the Form",
+  // },
 ];
