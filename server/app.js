fb18ad0db42b3d2af76f7260c64202f4f51f5e10
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-12 10:32:24 +0700

    changed local port

--- server/app.js
+++ server/app.js
@@ -77,5 +77,5 @@ app.get("/", (req, res) => {
 //--------------------------------------- END OF ROUTES -----------------------------------------//
 
 //Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
