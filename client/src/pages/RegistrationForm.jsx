7e2158144159a04525c8785762fd1470d4dc01f8
Fabio Jonathan Arifin <42570291+fabiojonathanarifin@users.noreply.github.com>
2022-11-12 12:20:18 +0700

    tested validation

--- client/src/pages/RegistrationForm.jsx
+++ client/src/pages/RegistrationForm.jsx
@@ -1,14 +1,36 @@
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 import LightGenButton from "../components/Buttons/Button/GeneralButton";
-
 function RegistrationForm() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  //test FE validation on email address
   return (
     <div className="mt-5">
       <h1>Registration Form</h1>
-      <Form>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            required
+            type="email"
+            placeholder="Enter email"
+            defaultValue="narj"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -26,9 +48,6 @@ function RegistrationForm() {
           <Form.Control type="location" placeholder="Seattle, Washington" />
         </Form.Group>
         <LightGenButton Value="Register" type="submit" />
-        {/* <Button variant="primary" type="submit">
-          Register
-        </Button> */}
       </Form>
     </div>
   );
